e70941b034a8593f03d88b2f3684082995d98274
hwchiu <hwchiu@cs.nctu.edu.tw>
2017-01-24 11:31:38 +0800

    time O(n), space O(1)
    - count the bit for each position.
    - sum of (C(n-pos[k],1) * C(pos[k],1)) for each position.
    
    Signed-off-by: hwchiu <hwchiu@cs.nctu.edu.tw>

---
+++ 477-Total_Hamming_Distance.c
@@ -0,0 +1,17 @@
+int totalHammingDistance(int* nums, int numsSize) {
+    int count[32] = {0}; //for 32-bit int.
+    int i, sum = 0;
+    for (i = 0; i < numsSize; ++i) {
+        int j = 0, num = nums[i];
+        while(num) {
+            count[j] += (num&0x01);
+            num >>=1;
+            j++;
+        }
+    }
+    for (i = 0; i < 32; i++) {
+        sum += count[i] * (numsSize - count[i]);
+    } 
+    return sum;
+}
+
